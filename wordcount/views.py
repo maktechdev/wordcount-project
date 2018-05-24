9665c9d64ed12c3001541d6455a6b8256b713a90
Moby Khan <mub541@hotmail.com>
2018-05-24 20:13:49 +0200

    Initial Commit.

---
+++ wordcount/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from datetime import datetime as dt
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html', {'time':dt.today()})
+
+def count(request):
+    return render(request, 'count.html', name='count')
